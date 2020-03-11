d3c17f661c87517e79c42f5458a2fcb697b28e85
Andres Lujan <alujan@hugeinc.com>
2020-03-10 22:30:24 -0500

    WIP

--- abstract.py
+++ abstract.py
@@ -1,6 +1,6 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
-class MyABC:
+class MyABC(object):
     """Absctract base class definition"""
     __metaclass__ = ABCMeta
 
@@ -11,3 +11,15 @@ class MyABC:
     @abstractproperty
     def some_property(self):
         """Required property"""
+
+
+class MyClass(MyABC):
+    def __init__(self, value=None):
+        self._myprop = value
+    
+    def do_something(self, value):
+        self._myprop *= 2 + value
+    
+    @property
+    def some_property(self):
+        return self._myprop
